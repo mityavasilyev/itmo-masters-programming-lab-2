8385a5d714330e508ab3700e63501045ee93774f
Dmitry <contact.dmitry.vasilyev@gmail.com>
2024-04-13 14:45:07 +0300

    More tweaks to tests

--- test_commands.sh
+++ test_commands.sh
@@ -14,23 +14,33 @@ test_command() {
     fi
 }
 
+generate_basic_auth_header() {
+    local username=$1
+    local password=$2
+    local auth_string="$username:$password"
+    local auth_base64=$(echo -n "$auth_string" | base64)
+    echo "Authorization: Basic $auth_base64"
+}
+
 # Test commands
 # Create user
-test_command "curl --location 'localhost:8080/api/users' --header 'Content-Type: application/json' --data '{\"username\": \"boba\",\"password\": \"boba\"}'" "{\"id\": 7,\"username\": \"boba\"}"
+test_command "curl --location 'localhost:8080/api/users' --header 'Content-Type: application/json' --data '{\"username\": \"boba\",\"password\": \"boba\"}'" "{\"id\": [0-9]+,\"username\": \"boba\"}"
+basic_auth_header=$(generate_basic_auth_header "boba" "boba")
 
 # Login user
-test_command "curl --location --request POST 'localhost:8080/api/users/login' --header 'Authorization: Basic Ym9iYTpib2Jh' --data ''" "{\"username\": \"boba\",\"isLoginSuccessful\": true}"
+test_command "curl --location --request POST 'localhost:8080/api/users/login' --header '$basic_auth_header' --data ''" "{\"username\": \"boba\",\"isLoginSuccessful\": true}"
 
 # Send message
-test_command "curl --location 'localhost:8080/api/messages' --header 'Content-Type: application/json' --header 'Authorization: Basic cGlkcjpwaWRy' --data '{\"content\": \"Privet loshara\",\"receiverUsername\": \"boba\"}'" "HTTP CODE 200"
+test_command "curl --location 'localhost:8080/api/messages' --header 'Content-Type: application/json' --header '$basic_auth_header' --data '{\"content\": \"Privet loshara\",\"receiverUsername\": \"loshara\"}'" "HTTP CODE 200"
 
 # Verify sent messages
-test_command "curl --location --request POST 'localhost:8080/api/messages/sent' --header 'Authorization: Basic cGlkcjpwaWRy' --header 'Cookie: JSESSIONID=BCF7925F43CA992DDA6058165165B3E7'" "{\"id\": 1,\"content\": \"Privet pidr\",\"senderUsername\": \"pidr\",\"receiverUsername\": \"boba\"}"
+test_command "curl --location --request POST 'localhost:8080/api/messages/sent' --header '$basic_auth_header' --header 'Cookie: JSESSIONID=BCF7925F43CA992DDA6058165165B3E7'" "{\"id\": [0-9]+,\"content\": \"Privet loshara\",\"senderUsername\": \"boba\",\"receiverUsername\": \"loshara\"}"
 
 # Create another user
-test_command "curl --location 'localhost:8080/api/users' --header 'Content-Type: application/json' --data '{\"username\": \"newuser\",\"password\": \"newuser\"}'" "{\"id\": 8,\"username\": \"newuser\"}"
+test_command "curl --location 'localhost:8080/api/users' --header 'Content-Type: application/json' --data '{\"username\": \"loshara\",\"password\": \"loshara\"}'" "{\"id\": [0-9]+,\"username\": \"loshara\"}"
+basic_auth_header=$(generate_basic_auth_header "loshara" "loshara")
 
 # Verify received messages
-test_command "curl --location --request POST 'localhost:8080/api/messages/received' --header 'Authorization: Basic cGlkcjpwaWRy' --header 'Cookie: JSESSIONID=BCF7925F43CA992DDA6058165165B3E7'" "{\"id\": 5,\"content\": \"Privet loshara\",\"senderUsername\": \"pidr\",\"receiverUsername\": \"boba\"}"
+test_command "curl --location --request POST 'localhost:8080/api/messages/received' --header '$basic_auth_header' --header 'Cookie: JSESSIONID=BCF7925F43CA992DDA6058165165B3E7'" "{\"id\": [0-9]+,\"content\": \"Privet loshara\",\"senderUsername\": \"boba\",\"receiverUsername\": \"loshara\"}"
 
 echo "All tests passed."
